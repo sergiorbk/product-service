467c70003ce7b4d1bc5ccc51d9ec60631d779234
sergiorbk <sergey24rybak@gmail.com>
2024-10-30 00:23:21 +0200

    CategoryCreateDto was implemented; DTO classes templates for Product and Order were created.

---
+++ src/main/java/com/sergosoft/productservice/dto/order/OrderCreateDto.java
@@ -0,0 +1,10 @@
+package com.sergosoft.productservice.dto.order;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class OrderCreateDto {
+    // todo implement
+}
---
+++ src/main/java/com/sergosoft/productservice/dto/product/ProductCreateDto.java
@@ -0,0 +1,10 @@
+package com.sergosoft.productservice.dto.product;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class ProductCreateDto {
+    // todo implement
+}
