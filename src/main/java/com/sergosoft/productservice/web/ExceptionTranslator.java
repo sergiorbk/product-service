94619a275f9aedb117912ef35915c42a0650d604
sergiorbk <sergey24rybak@gmail.com>
2024-10-31 22:57:01 +0200

    Exception Handler for fields validators was implemented;

--- src/main/java/com/sergosoft/productservice/web/ExceptionTranslator.java
+++ src/main/java/com/sergosoft/productservice/web/ExceptionTranslator.java
@@ -1,21 +1,37 @@
 package com.sergosoft.productservice.web;
 
 import java.net.URI;
+import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
+import lombok.NonNull;
+import org.springframework.http.*;
+import org.springframework.validation.FieldError;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import lombok.extern.slf4j.Slf4j;
 
+import com.sergosoft.productservice.service.exception.OrderNotFoundException;
+import com.sergosoft.productservice.service.exception.ParamsViolationDetails;
+import com.sergosoft.productservice.service.exception.ProductNotFoundException;
 import com.sergosoft.productservice.service.exception.OrderItemNotFoundException;
 import com.sergosoft.productservice.service.exception.category.CategoryNotFoundException;
 import com.sergosoft.productservice.service.exception.category.ParentCategoryNotFoundException;
 
 @ControllerAdvice
 @Slf4j
-public class ExceptionTranslator {
+public class ExceptionTranslator extends ResponseEntityExceptionHandler {
+
+    public static ProblemDetail getValidationErrorsProblemDetail(List<ParamsViolationDetails> validationResponse) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Request validation failed");
+        problemDetail.setType(URI.create("urn:problem-type:validation-error"));
+        problemDetail.setTitle("Field Validation Exception");
+        problemDetail.setProperty("invalidParams", validationResponse);
+        return problemDetail;
+    }
 
     @ExceptionHandler(CategoryNotFoundException.class)
     ProblemDetail handleCategoryNotFoundException(CategoryNotFoundException ex) {
@@ -42,5 +58,35 @@ public class ExceptionTranslator {
         problemDetail.setType(URI.create("order-item-not-found"));
         problemDetail.setTitle("Order Item Not Found");
         return problemDetail;
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    ProblemDetail handleOrderNotFoundException(OrderNotFoundException ex) {
+        log.info("Order Not Found exception raised.");
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setType(URI.create("order-not-found"));
+        problemDetail.setTitle("Order Not Found");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    ProblemDetail handleProductNotFoundException(ProductNotFoundException ex) {
+        log.info("Product Not Found exception raised.");
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setType(URI.create("product-not-found"));
+        problemDetail.setTitle("Product Not Found");
+        return problemDetail;
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(@NonNull MethodArgumentNotValidException ex,
+                                                                  @NonNull HttpHeaders headers,
+                                                                  @NonNull HttpStatusCode status,
+                                                                  @NonNull WebRequest request) {
+        List<FieldError> errors = ex.getBindingResult().getFieldErrors();
+        List<ParamsViolationDetails> validationResponse =
+                errors.stream().map(err -> ParamsViolationDetails.builder().reason(err.getDefaultMessage()).fieldName(err.getField()).build()).toList();
+        log.info("Input params validation failed");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getValidationErrorsProblemDetail(validationResponse));
     }
 }
