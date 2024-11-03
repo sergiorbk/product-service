3153c6b552a59d55b565295eecf7085921563d0a
sergiorbk <sergey24rybak@gmail.com>
2024-11-03 19:51:35 +0200

    Annotations for service layer classes were added

--- src/test/java/com/sergosoft/productservice/service/CategoryServiceTests.java
+++ src/test/java/com/sergosoft/productservice/service/CategoryServiceTests.java
@@ -1,9 +1,17 @@
 package com.sergosoft.productservice.service;
 
+import com.sergosoft.productservice.config.MappersTestConfiguration;
 import com.sergosoft.productservice.service.impl.CategoryServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest(classes = {CategoryServiceImpl.class})
+@Import(MappersTestConfiguration.class)
+@DisplayName("Category Service Tests")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CategoryServiceTests {
     // todo
 }
