d9f1af8e0009151f3b7c2ad99793083a644500ed
sergiorbk <sergey24rybak@gmail.com>
2024-11-04 15:59:45 +0200

    Added @Builder annotation to CurrencyRate

--- src/main/java/com/sergosoft/productservice/domain/CurrencyRate.java
+++ src/main/java/com/sergosoft/productservice/domain/CurrencyRate.java
@@ -1,10 +1,12 @@
 package com.sergosoft.productservice.domain;
 
+import lombok.Builder;
 import lombok.Value;
 
 import java.math.BigDecimal;
 
 @Value
+@Builder
 public class CurrencyRate {
 
     String currencyCode;
