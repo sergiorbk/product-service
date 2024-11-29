c8f47747614461392e3f3d4eec9198ff922b8994
Serhii Rybak <sergey24rybak@gmail.com>
2024-11-29 14:51:06 +0200

    NaturalRepositoryImpl hotfix

--- src/main/java/com/sergosoft/productservice/ProductServiceApplication.java
+++ src/main/java/com/sergosoft/productservice/ProductServiceApplication.java
@@ -1,9 +1,12 @@
 package com.sergosoft.productservice;
 
+import com.sergosoft.productservice.repository.impl.NaturalRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = NaturalRepositoryImpl.class)
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sergosoft/productservice/repository/ProductRepository.java
+++ src/main/java/com/sergosoft/productservice/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.sergosoft.productservice.repository;
 import com.sergosoft.productservice.repository.entity.ProductEntity;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ProductRepository extends NaturalIdRepository<ProductEntity, String> {
+public interface ProductRepository extends NaturalRepository<ProductEntity, UUID> {
 
 }
