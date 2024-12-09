bca92d5ed8c1e0939e8234791ba875b9a673d2b5
Serhii Rybak <sergey24rybak@gmail.com>
2024-12-09 19:10:50 +0200

    Security filter chains were partly implemented in SecurityConfiguration; Resource server was configured to get the JWK set from the authorization server (Keycloak)

--- src/main/java/com/sergosoft/productservice/config/security/SecurityConfiguration.java
+++ src/main/java/com/sergosoft/productservice/config/security/SecurityConfiguration.java
@@ -1,13 +1,66 @@
 package com.sergosoft.productservice.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
 
+    private final String API_V1_PRODUCTS = "/api/v1/products/**";
+    private final String API_V1_CATEGORIES = "/api/v1/categories/**";
+    private final String API_V1_ORDERS = "/api/v1/orders/**";
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain filterChainProducts(HttpSecurity http) throws Exception {
+        http.securityMatcher(API_V1_PRODUCTS)
+                .cors(Customizer.withDefaults())
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        authorize -> authorize.requestMatchers(API_V1_PRODUCTS).authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain filterChainCategories(HttpSecurity http) throws Exception {
+        http.securityMatcher(API_V1_CATEGORIES)
+        .csrf(CsrfConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        authorize -> authorize.requestMatchers(API_V1_CATEGORIES).authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(3)
+    public SecurityFilterChain filterChainOrders(HttpSecurity http) throws Exception {
+        http.securityMatcher(API_V1_ORDERS)
+        .csrf(CsrfConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        authorize -> authorize.requestMatchers(API_V1_ORDERS).authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
 
 }
