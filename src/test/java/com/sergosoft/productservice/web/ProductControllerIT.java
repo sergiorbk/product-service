45aff73f718116db88e3bf29fc4666910076bc9c
Serhii Rybak <sergey24rybak@gmail.com>
2024-12-12 19:59:14 +0200

    Commented code was removed from ProductControllerIT

--- src/test/java/com/sergosoft/productservice/web/ProductControllerIT.java
+++ src/test/java/com/sergosoft/productservice/web/ProductControllerIT.java
@@ -95,17 +95,10 @@ class ProductControllerIT extends IntegrationTest {
 
     @Test
     void shouldUpdateProduct() throws Exception {
-//        CategoryCreateDto categoryCreateDto = CategoryCreateDto.builder()
-//                .title(testCategoryDetails.getTitle() + " child " + RandomStringUtils.randomAlphabetic(5))
-//                .parentId(testCategoryDetails.getId().toString())
-//                .build();
-//        CategoryDetails childCategoryDetails = categoryService.createCategory(categoryCreateDto);
-
         ProductUpdateDto productUpdateDto = ProductUpdateDto.builder()
                 .title(RandomStringUtils.randomAlphabetic(5))
                 .description(RandomStringUtils.randomAlphabetic(5))
                 .price(BigDecimal.valueOf(888))
-//                .categoryIds(List.of(childCategoryDetails.getId().toString()))
                 .build();
 
         mockMvc.perform(put("/api/v1/products/{id}", testProductDetails.getId())
@@ -117,8 +110,6 @@ class ProductControllerIT extends IntegrationTest {
                 .andExpect(jsonPath("$.title").value(productUpdateDto.getTitle()))
                 .andExpect(jsonPath("$.description").value(productUpdateDto.getDescription()))
                 .andExpect(jsonPath("$.price").value(productUpdateDto.getPrice()));
-//                .andExpect(jsonPath("$.categoriesIds").exists())
-//                .andExpect(jsonPath("$.categoriesIds").isArray());
     }
 
     @Test
