8301f05b025ecd4665e0003fe50ddaf7c225b436
Serhii Rybak <sergey24rybak@gmail.com>
2024-12-15 04:04:54 +0200

    Some useless code was removed

--- src/main/java/com/sergosoft/productservice/repository/CategoryRepository.java
+++ src/main/java/com/sergosoft/productservice/repository/CategoryRepository.java
@@ -1,7 +1,6 @@
 package com.sergosoft.productservice.repository;
 
 import com.sergosoft.productservice.repository.entity.CategoryEntity;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +9,6 @@ import java.util.UUID;
 @Repository
 public interface CategoryRepository extends NaturalIdRepository<CategoryEntity, UUID, String> {
 
-    @Cacheable("rootCategories")
     List<CategoryEntity> findByParentNull();
 
     List<CategoryEntity> findBySlugIn(List<String> slugList);
